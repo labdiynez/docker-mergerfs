14b8976945ecd82f41ea766f8547f44a19f7127f
hotio <26902309+hotio@users.noreply.github.com>
2020-05-28 10:58:24 +0200

    build unstable

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
 else
-    version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/trapexit/mergerfs/releases/latest" | jq -r .tag_name | sed s/v//g)
+    version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/trapexit/mergerfs/commits/master" | jq -r .sha)
     [[ -z ${version} ]] && exit 1
     sed -i "s/{MERGERFS_VERSION=[^}]*}/{MERGERFS_VERSION=${version}}/g" .drone.yml
     echo "##[set-output name=version;]${version}"
